e1477b5cb4ba8f7f6687464ea14e2881fe1b3a01
MlikeW <mayinlost@gmail.com>
2020-05-12 19:12:43 +0300

    lab

---
+++ TwoPhaseCommitTransactions/classes/DbUtilities.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Npgsql;
+
+namespace TwoPhaseCommitTransactions
+{
+    static class DbUtilities
+    {
+        public static NpgsqlConnection GetAndOpenConnection(string dbName)
+        {
+            var connection =
+                new NpgsqlConnection(
+                    $"Server=localhost; Port=5432; User Id=postgres; Password=110798; Database={dbName}");
+            connection.Open();
+            return connection;
+        }
+
+        public static T ExecuteCommandAndGetAnswer<T>(this NpgsqlConnection connection, string command,
+            string column = default)
+        {
+            var cmd = new NpgsqlCommand(command, connection);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            T variable = default;
+            if (!string.IsNullOrEmpty(column))
+            {
+                while(dr.Read())
+                {
+                    variable = (T) dr[column];
+                }
+            }
+
+            dr.Close();
+            return variable;
+        }
+
+        public static void InsertIntoTable(this NpgsqlConnection connection, string tableWithRows,
+            string values)
+            => connection.ExecuteCommandAndGetAnswer<string>($"INSERT INTO {tableWithRows} VALUES {values}");
+
+        public static void UpdateTable(this NpgsqlConnection connection, string table,
+            string columnsAndValues, string condition)
+            => connection.ExecuteCommandAndGetAnswer<string>($"UPDATE {table} SET {columnsAndValues} WHERE {condition}");
+
+        public static T SelectFromTable<T>(this NpgsqlConnection connection, string table, string condition, string column = default,
+            string rows = "*")
+            => connection.ExecuteCommandAndGetAnswer<T>($"SELECT {rows} FROM {table} WHERE {condition}", column);
+
+        public static T SelectAllFromTable<T>(this NpgsqlConnection connection, string table, string column = default)
+            => connection.ExecuteCommandAndGetAnswer<T>($"SELECT * FROM {table}", column);
+
+        public static void PrepareTransaction(this NpgsqlConnection connection, string name)
+            => connection.ExecuteCommandAndGetAnswer<string>($"PREPARE TRANSACTION '{name}'");
+
+        public static void CommitPrepared(this NpgsqlConnection connection, string name)
+            => connection.ExecuteCommandAndGetAnswer<string>($"COMMIT PREPARED '{name}'");
+
+        public static void RollbackPrepared(this NpgsqlConnection connection, string name)
+            => connection.ExecuteCommandAndGetAnswer<string>($"ROLLBACK PREPARED '{name}'");
+
+        public static string ToDate(this string date) => $"TO_DATE('{date}', 'YYYY-MM-DD')";
+
+        public static string Minus(this string availabillity, int i = 1) => $"{availabillity} = {availabillity} - {i}";
+
+        public static void CreateFlyTable(this NpgsqlConnection connection)
+            => connection.ExecuteCommandAndGetAnswer<string>("CREATE TABLE FlyTable(" +
+                                                             "FromCity varchar(255)," +
+                                                             "ToCity varchar(255)," +
+                                                             "DateTicket date," +
+                                                             "price int," +
+                                                             "availabillity int CHECK(availabillity >= 0)" +
+                                                             ");");
+
+        public static void CreateHotelTable(this NpgsqlConnection connection)
+            => connection.ExecuteCommandAndGetAnswer<string>("CREATE TABLE LondonCoolHotel(" +
+                                                             "roomnum int primary key," +
+                                                             "roomprice int," +
+                                                             "roomfrom date," +
+                                                             "roomto date," +
+                                                             "availabillity int CHECK(availabillity >= 0)" +
+                                                             ");");
+
+        public static void DropTableIfExist(this NpgsqlConnection connection, string table)
+        {
+            try
+            {
+                connection.ExecuteCommandAndGetAnswer<string>($"DROP TABLE {table}");
+            }
+            catch
+            {
+                Console.Write($"There was no table '{table}'");
+            }
+        }
+    }
+}
---
+++ TwoPhaseCommitTransactions/classes/TwoPC.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TwoPhaseCommitTransactions.classes
+{
+    [TestFixture]
+    public class TwoPC
+    {
+        private const string FlyTransactionName = "FlyTransaction";
+        private const string HotelTransactionName = "HotelTransaction";
+
+        [Test]
+        public void TestInsertIntoFly()
+        {
+            var flyDb = DbUtilities.GetAndOpenConnection("FlyBooking");
+            flyDb.DropTableIfExist("FlyTable");
+            flyDb.CreateFlyTable();
+            flyDb.InsertIntoTable("FlyTable", $"('Kyiv', 'London', {"2020-10-10".ToDate()}, 50, 1)");
+            flyDb.InsertIntoTable("FlyTable", $"('Kyiv', 'London', {"2020-10-12".ToDate()}, 50, 1)");
+            flyDb.InsertIntoTable("FlyTable", $"('London', 'Kyiv', {"2020-10-15".ToDate()}, 50, 2)");
+
+            flyDb.Close();
+        }
+
+        [Test]
+        public void TestInsertIntoHotel()
+        {
+            var hotelDb = DbUtilities.GetAndOpenConnection("HotelBooking");
+            hotelDb.DropTableIfExist("LondonCoolHotel");
+            hotelDb.CreateHotelTable();
+            hotelDb.InsertIntoTable("LondonCoolHotel",
+                $"(121, 150, {"2020-10-12".ToDate()}, {"2020-10-15".ToDate()}, 1)");
+            hotelDb.InsertIntoTable("LondonCoolHotel",
+                $"(122, 150, {"2020-10-10".ToDate()}, {"2020-10-15".ToDate()}, 0)");
+
+            hotelDb.Close();
+        }
+
+        [Test]
+        public void TestAlmostFinalOk()
+        {
+            var flyDb = DbUtilities.GetAndOpenConnection("FlyBooking");
+            var hotelDb = DbUtilities.GetAndOpenConnection("HotelBooking");
+
+            var transHotel = hotelDb.BeginTransaction();
+            var transFly = flyDb.BeginTransaction();
+            try
+            {
+                var flyCondition1 = $"fromcity = 'Kyiv' AND tocity = 'London' AND dateticket = {"2020-10-12".ToDate()}";
+                var hotelCondition = $"roomfrom = {"2020-10-12".ToDate()} AND roomto = {"2020-10-15".ToDate()}";
+                var flyCondition2 = $"fromcity = 'London' AND tocity = 'Kyiv' AND dateticket = {"2020-10-15".ToDate()}";
+
+                var priceFly1 = flyDb.SelectFromTable<int>("FlyTable", flyCondition1, "price");
+                flyDb.UpdateTable("FlyTable", "availabillity".Minus(), $"{flyCondition1} AND price = {priceFly1}");
+                var priceHotel = hotelDb.SelectFromTable<int>("LondonCoolHotel", hotelCondition, "roomprice");
+                hotelDb.UpdateTable("LondonCoolHotel", "availabillity".Minus(), $"{hotelCondition} AND roomprice = {priceHotel}");
+                var priceFly2 = flyDb.SelectFromTable<int>("FlyTable", flyCondition2, "price");
+                flyDb.UpdateTable("FlyTable", "availabillity".Minus(), $"{flyCondition2} AND price = {priceFly2}");
+
+                flyDb.PrepareTransaction(FlyTransactionName);
+                hotelDb.PrepareTransaction(HotelTransactionName);
+
+                flyDb.CommitPrepared(FlyTransactionName);
+                hotelDb.CommitPrepared(HotelTransactionName);
+            }
+            catch (Exception ex)
+            {
+                transHotel.Rollback();
+                transFly.Rollback();
+                Console.WriteLine($"\nTransaction Failed: \n{ex.Message}");
+            }
+
+            if (flyDb.SelectAllFromTable<string>("pg_prepared_xacts", "gid") != null)
+            {
+                throw new Exception("Prepared Transaction Table not empty");
+            }
+
+            flyDb.Close();
+            hotelDb.Close();
+            Console.WriteLine("\nTransaction Successful");
+        }
+    }
+}
